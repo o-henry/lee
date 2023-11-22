9565ecbce92f05875dda45ff9c2e230e881bba97
henry <c.henry.9209@gmail.com>
2023-11-22 13:26:11 +0900

    fix: animation ... ing

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'hang.out.blog',
-  author: 'hang.out',
+  title: 'hang.out',
+  author: 'henry',
   headerTitle: '',
-  description: 'Developer',
+  description: "I'm henry",
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
+  email: 'c.henry.9209@gmail.com',
   github: 'https://github.com',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
