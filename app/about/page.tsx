ab7be2a36e9430483e22957a72c4ea1ca2471984
Henry <c.henry.9209@gmail.com>
2024-10-03 18:22:36 +0900

    [UPLOAD] : v.1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({ title: 'ABOUT' })
 
 export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
--- components/Header.tsx
+++ components/Header.tsx
@@ -12,22 +12,24 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            {/* <div className="mr-3 font-bantayog lowercase">
-              Henry
-            </div> */}
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
       </div>
       <div className="flex font-bantayog lowercase items-center space-x-4 leading-5 sm:space-x-6">
+        <div>
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              <div className="mr-1 font-bantayog lowercase">
+                home
+              </div>
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
+        </div>
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link) => (
